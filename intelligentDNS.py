8599b3dba6a9544ac609d73cc5b77091723b6599
root <root@server-b3-1.maas>
2023-07-12 08:59:16 +0000

    bug fix

--- intelligentDNS.py
+++ intelligentDNS.py
@@ -1,6 +1,7 @@
 import socket
 from threading import Thread
 from scapy.all import *
+import paho.mqtt.subscribe as subscribe
 
 class IntelligentDNS():
     def __init__(self):
@@ -9,23 +10,37 @@ class IntelligentDNS():
         self.DNS_PORT = 53
         self.dns_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.dns_socket.bind((self.DNS_IP, self.DNS_PORT)) 
+        self.UE_address = ["60.60.0.1", "60.60.0.2", "60.60.0.3", "60.60.0.4", "10.0.0.24"]
         self.UE_DNS_MAPPING = {
-            "domain": ["www.h51.com.", "www.h57.com.", "www.h51.com."],
-            "dns_record": ["10.20.1.51", "10.20.1.57", "10.20.1.57"],
-            "UE": ["10.20.1.58", "10.0.0.203", "10.0.0.203"]
+            "domain": ["www.mec-app.com.", "www.mec-app.com.", "www.mec-app.com.", "www.mec-app.com.", "www.mec-app.com."],
+            "dns_record": ["10.20.1.58", "10.20.1.58", "10.20.1.51", "10.20.1.58", "10.20.1.58"],
+            "UE" : self.UE_address
         }
         
     def run(self):
         t = Thread(target=self.__dns_server__)
         t.start()
+        t1 = Thread(target=self.__mqtt_dns__)
+        t1.start()
+
+    def __mqtt_dns__(self):
+        msg = subscribe.simple("free5gc/DNS", hostname="10.0.0.218")
+        msg = msg.payload.decode('utf-8')
+        UE_address = self.UE_DNS_MAPPING["UE"]
+        UE_address.append(msg)
+        self.UE_DNS_MAPPING["UE"] = UE_address
+        print(UE_address)
 
     def __dns_server__(self):
         while True:
             data, address = self.dns_socket.recvfrom(self.BUFFER_SIZE)
+            print(address[0])
             if address[0] in self.UE_DNS_MAPPING["UE"]:
                 print(address[0])
             data = DNS(data)
+
             answer_record = self.__search_record__(domain_name = data.qd.qname.decode('utf-8'), source_ip = address[0])
+            print(answer_record)
             answer = DNSRR(rrname=data.qd.qname, ttl=5, rdlen=4, rdata=answer_record)
             data.ancount = 1
             data.qr=1
@@ -34,6 +49,7 @@ class IntelligentDNS():
             self.dns_socket.sendto(bytes(data), address)
    
     def __search_record__(self, source_ip, domain_name):
+        print(source_ip, domain_name)
         ue_dns_mapping = {"domain": [], "dns_record": [], "UE": []}
         try:
             for domain in range(len(self.UE_DNS_MAPPING["domain"])):
